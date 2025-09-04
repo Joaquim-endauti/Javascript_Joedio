0f6136574ea91ac6d52ee40ce01bd576973b441d
Joaquim-endauti <joaquimdeoliveiramdossantos@gmail.com>
2025-09-04 11:13:15 -0300

    Add files via upload

--- nodejs/modulos/https/app.js
+++ nodejs/modulos/https/app.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World');
+    res.end('Olá, Mundo!');
 }).listen(8080);
