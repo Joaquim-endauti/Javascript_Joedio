8394835d2aec9910fc4775142292846d7c42a3ab
Aluno IFC <Joaquim>
2025-10-14 09:19:22 -0300

    Versão com formulário com elo do lol

---
+++ nodejs/saudacao-mvc/controllers/siteController.js
@@ -0,0 +1,18 @@
+const saudacaoModel = require('../models/saudacaoModel');
+
+module.exports = {
+  index: (req, res) => {
+    res.sendFile('index.html', { root: './views' });
+  },
+
+  formulario: (req, res) => {
+    res.sendFile('formulario.html', { root: './views' });
+  },
+
+  saudacao: (req, res) => {
+    const { nome, idade, elo } = req.body;
+    const mensagem = saudacaoModel.gerarMensagemPersonalizada(nome, idade, elo);
+    res.send(`<h1>${mensagem}</h1>`);
+  }
+
+};
